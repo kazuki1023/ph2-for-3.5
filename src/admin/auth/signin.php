587c4d056d6aa1e6d90de27d47e1101ba4c5a7a3
kazuki1023 <k.keio12569@gmail.com>
2023-10-03 16:13:30 +0900

    add ログイン後に管理者画面に飛ぶ

--- src/admin/auth/signin.php
+++ src/admin/auth/signin.php
@@ -18,6 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $_SESSION['id'] = $user["id"];
     $_SESSION['name'] = $user["name"];
     $message = "ログインに成功しました";
+    header("Location: /admin/index.php");
   }
 }
 ?>
